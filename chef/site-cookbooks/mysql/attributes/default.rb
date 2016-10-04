7ba79cb413dd23ae8891cb2768e8b94a309973ba
okamuroshogo <okamuro-shogo@kayac.com>
2016-10-04 15:00:02 +0900

    update .gitignore

--- chef/site-cookbooks/mysql/attributes/default.rb
+++
@@ -1,8 +0,0 @@
-# rpm files
-default['mysql']['rpm'] = [
-  'MySQL-server-5.6.15-1.el6.x86_64.rpm',
-  'MySQL-client-5.6.15-1.el6.x86_64.rpm',
-  'MySQL-devel-5.6.15-1.el6.x86_64.rpm',
-  'MySQL-shared-5.6.15-1.el6.x86_64.rpm',
-  'MySQL-shared-compat-5.6.15-1.el6.x86_64.rpm'
-]
